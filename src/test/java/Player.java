8871ff0eb6e0c5a4e29c8c08a5b9df3a4dcebce9
nkurmanalieva <kurmanalieva.nurs@gmail.com>
2022-05-09 23:59:31 +0200

    Player test

---
+++ src/test/java/Player.java
@@ -0,0 +1,16 @@
+import model.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Player {
+
+    @Before
+    void setup(){}
+
+    @After
+    void shutdown() {}
+
+    @Test
+    public void test(){}
+}
