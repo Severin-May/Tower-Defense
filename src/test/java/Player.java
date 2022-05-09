f67dc9512513d898bdcf60449489f950a05ebaea
nkurmanalieva <kurmanalieva.nurs@gmail.com>
2022-05-10 00:21:09 +0200

    Player test decrease gold

--- src/test/java/Player.java
+++ src/test/java/Player.java
@@ -7,9 +7,11 @@ import static org.junit.Assert.assertEquals;
 
 public class Player {
     model.Player pl1;
+    model.Player pl2;
     @Before
     public void setup(){
         pl1 = Game.getInstance().getPlayer1(); //1500
+        pl2 = Game.getInstance().getPlayer2();
     }
 
     @After
@@ -19,5 +21,11 @@ public class Player {
     public void testIncreaseGold(){
         pl1.increaseGold(100);
         assertEquals(pl1.getGold(), 1600);
+    }
+
+    @Test
+    public void testDecreaseGold(){
+        pl2.decreaseGold(100);
+        assertEquals(pl2.getGold(), 1400);
     }
 }
